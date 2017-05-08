4886e65084a0a2a19b0858fe54d3b8bb90bfe70a
CharlesFrere <charles.frere@student.uclouvain.be>
2017-05-08 16:09:06 +0200

    Ajout de la classe ChoixContraintes

--- app/src/main/java/com/ezmeal/groupeh/ezmeal/ChoixContraintes.java
+++ app/src/main/java/com/ezmeal/groupeh/ezmeal/ChoixContraintes.java
@@ -3,8 +3,14 @@ package com.ezmeal.groupeh.ezmeal;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 
 /**
@@ -15,10 +21,36 @@ public class ChoixContraintes extends BaseActivity{
     //String contrainte;
     //String préférence1, préférence2, préférence3;
     //String indésiré1, indésiré2, indésiré3;
-
+    ArrayList<String> selectedItems = new ArrayList<>();
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.choix_contraintes);
+        ListView ch1 = (ListView)findViewById(R.id.checkable_list);
+        ch1.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        String[] items = {"Musulman","Juif","Végétarien","Vegan"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.checkable_list);
+        ch1.setAdapter(adapter);
+        ch1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItem = ((TextView)view).getText().toString();
+                if(selectedItems.contains(selectedItem)){
+                    selectedItems.remove(selectedItem);
+                }
+                else
+                    selectedItems.add(selectedItem);
+            }
+        });
+
+    }
+
+    public void showSelectedItems(View v){
+        String items= "";
+        for(String item:selectedItems){
+            items +="-"+item+"\n";
+        }
+        Toast.makeText(this, "Vous avez choisi vos contraintes \n"+items,Toast.LENGTH_LONG).show();
+
     }
 
 
@@ -51,7 +83,7 @@ public class ChoixContraintes extends BaseActivity{
             TextView indesire2 = (EditText) findViewById(R.id.editText6);
             TextView indesire3 = (EditText) findViewById(R.id.editText5);
 
-            TextView contrainte = (EditText) findViewById(R.id.ETalergie);
+            //TextView contrainte = (EditText) findViewById(R.id.liste_contraintes);
 
 
             String pref1 = preference1.getText().toString();
@@ -62,12 +94,12 @@ public class ChoixContraintes extends BaseActivity{
             String ind2 = indesire2.getText().toString();
             String ind3 = indesire3.getText().toString();
 
-            String contr = contrainte.getText().toString();
+            //String contr = contrainte.getText().toString();
 
             // comment enregister les contraintes ? aller chercher le User uMod de Compte ?
 
             User user = new User();
-            user.setContraintes(contr);
+            //user.setContraintes(contr);
             user.setPreferences(pref1,pref2,pref3);
             user.setIndésirés(ind1,ind2,ind3);
 
